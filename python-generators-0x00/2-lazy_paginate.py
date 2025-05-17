b00f6e8834e031174f91cc53789a022f81d57ffa
peterkahumu <muhumukip@gmail.com>
2025-05-17 17:35:08 +0300

    Lazy loading

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,33 @@
+from seed import connect_db
+
+def paginate_users(connection, page_size, offset):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (page_size, offset))
+        return cursor.fetchall()
+
+def lazy_paginate(page_size):
+    connection = connect_db()
+    if not connection:
+        raise Exception("Cannot connect to database.")
+    
+    try:
+        offset = 0
+        while True:  # ✅ Only 1 loop
+            page = paginate_users(connection, page_size, offset)
+            if not page:
+                break
+            yield page
+            offset += page_size
+    except Exception as e:
+        raise Exception("❌ Error during pagination:", e)
+    finally:
+        connection.close()
+
+if __name__ == '__main__':
+    try:
+        for page in lazy_paginate(2):
+            for user in page:
+                print(user)
+            print("---- NEXT PAGE ----\n")
+    except Exception as e:
+        print(e)
