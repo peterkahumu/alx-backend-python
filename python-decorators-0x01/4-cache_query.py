4cd31afe3dc2404d310293c7a3328d1d4653484d
peterkahumu <muhumukip@gmail.com>
2025-05-23 22:23:35 +0300

    Cache queries

---
+++ python-decorators-0x01/4-cache_query.py
@@ -0,0 +1,28 @@
+from db_connection import with_db_connection
+from datetime import datetime
+
+query_cache = {}
+
+def cache_query(func):
+    def wrapper(conn, query, *args, **kwargs):
+        if query in query_cache:
+            print('[cache] Returned cached Result')
+            return query_cache[query]
+        
+        result = func(conn , query, *args, **kwargs)
+        query_cache[query] = result
+        return result
+    return wrapper
+
+@with_db_connection
+@cache_query
+def fetch_users_with_cache(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+# First call caches the result
+users = fetch_users_with_cache(query="SELECT * FROM 
+
+# Second call returns cached result
+users_again = fetch_users_with_cache(query="SELECT * FROM user_data")
