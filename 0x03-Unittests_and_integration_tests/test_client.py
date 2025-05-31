79cfadd9b405ffcdba1f1178a15a4197d66e12b2
peterkahumu <muhumukip@gmail.com>
2025-05-31 23:57:06 +0300

    Test has licence

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -77,6 +77,22 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get_json.assert_called_once_with(
                 "https://api.github.com/orgs/test-org/repos"
             )
+        
+    @parameterized.expand([
+        (
+            {"license": {"key": "my_license"}}, "my_license", True
+        ),
+        (
+            {"license": {"key": "other_license"}}, "my_license", False
+        ),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        Test has_license returns True when license matches,
+        otherwise False.
+        """
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
