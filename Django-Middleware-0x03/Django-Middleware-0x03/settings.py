f3e23137e24cc4f2100a1957204904c02a515e7e
peterkahumu <muhumukip@gmail.com>
2025-06-08 11:12:34 +0300

    Reconfigure checker for chats.middleware.RestrictAccessByTimeMiddleware

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
